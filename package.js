475801778000ca5ce483adfa4ad9404f7e0fe64e
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:14:01 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-label',
+  summary : 'Semantic UI - Label (official): Single component release of label',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Label.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
