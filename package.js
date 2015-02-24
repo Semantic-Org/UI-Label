e8310921e7e6413f49001f206070a3525be1f8af
Pusher Robot <jack@semantic-ui.com>
2015-02-24 12:10:33 -0500

    Updated component to version 1.10.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-label',
   summary : 'Semantic UI - Label: Single component release',
-  version : '1.9.3',
+  version : '1.10.1',
   git     : 'git://github.com/Semantic-Org/UI-Label.git',
 });
 
