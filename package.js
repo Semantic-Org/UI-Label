b053277b41e577255257b7cb618a25727dfb2102
Jack Lukic <jack@semantic-ui.com>
2017-02-21 14:23:49 -0500

    Updated component to version 2.2.9

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-label',
   summary : 'Semantic UI - Label: Single component release',
-  version : '2.2.8',
+  version : '2.2.9',
   git     : 'git://github.com/Semantic-Org/UI-Label.git',
 });
 
